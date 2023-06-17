4486fc97ee2ca65a6d3f101130b4cf0dda1b9143
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-06-18 01:03:08 +0530

    [Sandeep] Add: Check Employee Attendance

---
+++ EmployeeWage/EmployeeWage/EmployeeAttendance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeAttendance
+    {
+        //UC1-Check Employee Attendance
+        public static void EmployeeCheck()
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int employeeAttendence = random.Next(0, 2); //0 or 1
+            if (employeeAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Computation Problem");
+            //UC- Employee Attendance
+            EmployeeAttendance.EmployeeCheck();
+            Console.ReadLine();
+        }
+    }
+}
